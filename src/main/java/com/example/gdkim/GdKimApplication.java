375d22d8653e4edb1d3459708dc01249e93c3d9f
gdkim <oyammo@hanmail.net>
2021-02-27 11:20:06 +0900

    Initial commit

---
+++ src/main/java/com/example/gdkim/GdKimApplication.java
@@ -0,0 +1,13 @@
+package com.example.gdkim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdKimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GdKimApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gdkim/GdKimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gdkim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdKimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
