6ee724f46de16085f60c479e9e76179ce2cdae4c
gdkim <oyammo@hanmail.net>
2021-03-03 20:30:02 +0900

    파일추가

--- src/main/java/com/example/gdkim/config/JpaConfig.java
+++ src/main/java/com/example/gdkim/config/JpaConfig.java
@@ -1,4 +1,4 @@
-package com.example.gdkim.config.auth;
+package com.example.gdkim.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
